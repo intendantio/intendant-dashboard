42c1acb49642113e4460a1b324f4a5c5f4c7bcb7
Intendant <intendant@clementmerlet.fr>
2022-03-12 17:07:30 +0100

    Enable status system

--- src/screens/contents/System/List.js
+++ src/screens/contents/System/List.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import { Add, CheckCircle, Error, FlashOff, FlashOn, Light } from '@mui/icons-material'
-import { Typography, Paper, Grid, Card, Button, TableContainer, TableHead, TableRow, TablePagination, IconButton, Box, Divider, Pagination } from '@mui/material'
+import { CheckCircle, Error} from '@mui/icons-material'
+import { Typography, Paper, Grid, Card, Button, Box, Divider, Pagination } from '@mui/material'
 import Request from '../../../utils/Request'
 import Desktop from '../../../components/Desktop'
-import AddButton from '../../../components/views/AddButton'
-import TypeProduct from '../../../components/TypeProduct'
-import Package from '../../../../package.json'
 import Moment from 'moment'
 import Loading from '../../../components/Loading'
-
 
 class System extends React.Component {
 
@@ -17,6 +13,7 @@ class System extends React.Component {
         this.state = {
             product: "light",
             logs: [],
+            status: { web: { status: "ok", message: "" }, cloud: { status: "ok", message: "" }, market: { status: "ok", message: "" }, manager: { status: "ok", message: "" } },
             page: 0,
             loading: true,
             upgrade: false,
@@ -31,6 +28,8 @@ class System extends React.Component {
         let result = await new Request().get().fetch("/api/logs")
         let resultUpgrade = await new Request().get().fetch("/api/upgrade")
         let resultConfigurations = await new Request().get().fetch("/api/configurations")
+        let resultStatus = await fetch("https://status.intendant.io/")
+        let resultStatusJSON = await resultStatus.json()
         if (result.error) {
             this.props.setMessage(result.package + " : " + result.message)
         } else if (resultUpgrade.error) {
@@ -41,6 +40,7 @@ class System extends React.Component {
             this.setState({
                 upgrade: resultUpgrade.data.upgrade,
                 version: resultUpgrade.data.version,
+                status: resultStatusJSON,
                 currentVersion: resultConfigurations.data.version,
                 logs: result.data.map(log => {
                     log.dateTime = Moment(parseInt(log.date)).format("HH:mm")
@@ -70,55 +70,57 @@ class System extends React.Component {
                             this.state.upgrade ?
                                 <>
                                     <Grid item xs={12} md={10} lg={10} >
-                                        <Card variant='outlined' style={{ padding: 12, display:'flex', flexDirection:'row' }}  >
+                                        <Card variant='outlined' style={{ padding: 12, display: 'flex', flexDirection: 'row' }}  >
                                             <Typography variant='subtitle1' fontWeight='bold' >{"Intendant core, an update is available " + this.state.currentVersion + " > " + this.state.version}</Typography>
                                         </Card>
                                     </Grid>
                                     <Grid item xs={12} md={2} lg={2}>
-                                        <Card elevation={3} style={{height: '100%'}} >
-                                            <Button variant='contained' color='success' onClick={() => { this.upgrade() }} style={{ width: '100%', height: '100%', padding: 10,  flexDirection: 'row', display: 'flex' }}>
-                                                <Typography textAlign='center' style={{color:'white'}} fontWeight='bold' variant='subtitle1'>{String.capitalizeFirstLetter("upgrade")}</Typography>
+                                        <Card elevation={3} style={{ height: '100%' }} >
+                                            <Button variant='contained' color='success' onClick={() => { this.upgrade() }} style={{ width: '100%', height: '100%', padding: 10, flexDirection: 'row', display: 'flex' }}>
+                                                <Typography textAlign='center' style={{ color: 'white' }} fontWeight='bold' variant='subtitle1'>{String.capitalizeFirstLetter("upgrade")}</Typography>
                                             </Button>
                                         </Card>
                                     </Grid>
                                 </> : null
 
                         }
-                        <Grid item xs={12} md={12} lg={4} >
+                        <Grid item xs={12} md={12} lg={5} >
                             <Card variant='outlined' style={{ padding: 12 }}  >
                                 <Typography variant='subtitle1' fontWeight='bold' >Cloud service</Typography>
                                 <Typography variant='subtitle2' color="text.secondary" >Powered from Intendant.io</Typography>
                                 <Divider style={{ marginTop: 12, marginBottom: 12 }} />
                                 <Box style={{ display: 'flex', flexDirection: 'row', marginBottom: 12 }}>
-                                    <CheckCircle color='success' />
+                                    {this.state.status.web.status == "ok" ? <CheckCircle color='success' /> : this.state.status.web.status == "warning" ? <Error color='warning' /> : <Error color='error' />}
                                     <Typography style={{ alignSelf: 'center', marginLeft: 12 }} variant='body1' >{"Website"}</Typography>
+                                    <Typography style={{ alignSelf: 'center', marginLeft: 12 }} color="text.secondary" variant='body1' >{this.state.status.web.message}</Typography>
                                 </Box>
                                 <Box style={{ display: 'flex', flexDirection: 'row', marginBottom: 12 }}>
-                                    <Error color='warning' />
+                                    {this.state.status.cloud.status == "ok" ? <CheckCircle color='success' /> : this.state.status.cloud.status == "warning" ? <Error color='warning' /> : <Error color='error' />}
                                     <Typography style={{ alignSelf: 'center', marginLeft: 12 }} variant='body1' >{"Cloud system"}</Typography>
-                                    <Typography style={{ alignSelf: 'center', marginLeft: 12 }} color="text.secondary" variant='body1' >{"Maintenance is in progress"}</Typography>
+                                    <Typography style={{ alignSelf: 'center', marginLeft: 12 }} color="text.secondary" variant='body1' >{this.state.status.cloud.message}</Typography>
                                 </Box>
                                 <Box style={{ display: 'flex', flexDirection: 'row', marginBottom: 12 }}>
-                                    <CheckCircle color='success' />
+                                    {this.state.status.market.status == "ok" ? <CheckCircle color='success' /> : this.state.status.market.status == "warning" ? <Error color='warning' /> : <Error color='error' />}
                                     <Typography style={{ alignSelf: 'center', marginLeft: 12 }} variant='body1' >{"Market"}</Typography>
+                                    <Typography style={{ alignSelf: 'center', marginLeft: 12 }} color="text.secondary" variant='body1' >{this.state.status.market.message}</Typography>
                                 </Box>
                                 <Box style={{ display: 'flex', flexDirection: 'row', marginBottom: 12 }}>
-                                    <Error color='error' />
+                                    {this.state.status.manager.status == "ok" ? <CheckCircle color='success' /> : this.state.status.manager.status == "warning" ? <Error color='warning' /> : <Error color='error' />}
                                     <Typography style={{ alignSelf: 'center', marginLeft: 12 }} variant='body1' >{"Manager"}</Typography>
-                                    <Typography style={{ alignSelf: 'center', marginLeft: 12 }} color="text.secondary" variant='body1' >{"A shutdown of the service is underway"}</Typography>
+                                    <Typography style={{ alignSelf: 'center', marginLeft: 12 }} color="text.secondary" variant='body1' >{this.state.status.manager.message}</Typography>
                                 </Box>
                             </Card>
                         </Grid>
-                        <Grid item xs={12} md={12} lg={8} >
+                        <Grid item xs={12} md={12} lg={7} >
                             <Card variant='outlined' style={{ padding: 12 }}  >
                                 <Typography variant='subtitle1' fontWeight='bold' >Log</Typography>
                                 <Typography variant='subtitle2' color="text.secondary" >Local information</Typography>
                                 <Divider style={{ marginTop: 12, marginBottom: 12 }} />
                                 {
-                                    this.state.logs.slice(this.state.page * 20, (this.state.page + 1) * 20).map((log,index) => {
+                                    this.state.logs.slice(this.state.page * 20, (this.state.page + 1) * 20).map((log, index) => {
                                         return (
-                                            <Box key={index} style={{ display: 'flex', flexDirecton: 'row', marginBottom: 3 }}>
-                                                <Box style={{ background: log.type == "VERBOSE" ? '#00873D' : log.type == "WARNING" ? "#FFAA15" : "#FF4040", borderRadius: 3, paddingLeft: 5, paddingRight: 5 }}>
+                                            <Box key={index} style={{ display: 'flex', flexDirecton: 'row', marginBottom: 5 }}>
+                                                <Box style={{ background: log.type == "VERBOSE" ? '#00873D' : log.type == "WARNING" ? "#FFAA15" : "#FF4040", borderRadius: 3, paddingLeft: 5, paddingRight: 5, height: 'min-content' }}>
                                                     <Typography variant='body2' style={{ alignItems: 'center' }} >{log.dateTime}</Typography>
                                                 </Box>
                                                 <Typography variant='body2' color="text.secondary" style={{ marginLeft: 10 }}  >{log.message}</Typography>
