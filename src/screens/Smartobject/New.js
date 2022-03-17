515e7896ee0eddf5f3e3ce5e1a6c161fef7bc771
Intendant <88664185+intendantio@users.noreply.github.com>
2022-03-17 14:45:21 +0100

    Update New.js

--- src/screens/Smartobject/New.js
+++ src/screens/Smartobject/New.js
@@ -130,7 +130,7 @@ class NewSmartobject extends React.Component {
         } else if (this.state.package.submit.type == "oauth") {
             
             return (
-                <a href={this.state.package.submit.url + (this.state.package.submit.url.includes("?") ? "&" : "?")+  "reference=" + this.state.reference + this.getOauthSettings() + "&room=" + this.state.idRoom + "&redirect_uri=" + window.location.origin + "/admin/smartobject/oauth/" + this.props.match.params.idSmartobject}>
+                <a href={this.state.package.submit.url + (this.state.package.submit.url.includes("?") ? "&" : "?")+  "reference=" + this.state.reference + this.getOauthSettings() + "&room=" + this.state.idRoom + "&redirect_uri=" + window.location.origin + "/smartobject/oauth/" + this.props.match.params.idSmartobject}>
                     <Button size='large' variant='contained' style={{ height: '100%', textTransform: 'none' }} >
                         <Typography color='white' >
                             {this.state.package.submit.name}
