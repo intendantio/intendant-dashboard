296c9713fcca9d77c8a5cabf95eddf88f7a4185e
Intendant <88664185+intendantio@users.noreply.github.com>
2022-03-17 14:46:49 +0100

    Update Authentification.js

--- src/screens/Authentification.js
+++ src/screens/Authentification.js
@@ -140,8 +140,8 @@ class Authentification extends React.Component {
                                         :
                                         null
                                 }
-                                <TextField style={{ marginTop: 5, marginBottom: 5 }} value={this.state.login} fullWidth label="Login" autoComplete="current-login" inputProps={{ maxLength: 12 }} onChange={(event) => { this.setState({ login: event.nativeEvent.target.value }) }} />
-                                <TextField style={{ marginTop: 5, marginBottom: 5 }} value={this.state.password} fullWidth label="Password" type='password' autoComplete="current-login" inputProps={{ maxLength: 12 }} onChange={(event) => { this.setState({ password: event.nativeEvent.target.value }) }} />
+                                <TextField style={{ marginTop: 5, marginBottom: 5 }} autocorrect="off" autocapitalize="none" value={this.state.login} fullWidth label="Login" autoComplete="current-login" inputProps={{ maxLength: 12 }} onChange={(event) => { this.setState({ login: event.nativeEvent.target.value }) }} />
+                                <TextField style={{ marginTop: 5, marginBottom: 5 }} autocorrect="off" autocapitalize="none" value={this.state.password} fullWidth label="Password" type='password' autoComplete="current-login" inputProps={{ maxLength: 12 }} onChange={(event) => { this.setState({ password: event.nativeEvent.target.value }) }} />
                                 <Box style={{ display: 'flex', justifyContent: 'end', marginTop: 10 }}>
                                     <Button color='inherit' type='submit' variant='plain' on onSubmit={() => { this.login() }} onClick={() => { this.login() }}  >
                                         Connection
